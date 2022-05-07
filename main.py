6f5931be6b067aa222d7c9ddfedfa0b0b42adb60
Emile Villette <emile.villette@student.uclouvain.be>
2022-05-07 23:46:58 +0200

    started linear method

--- main.py
+++ main.py
@@ -13,7 +13,18 @@ def pageRankLinear(A, alpha, v):
     :return: a vector containing the importance scores of the nodes sorted in the same ordered as the input adjacency matrix A
     :rtype: np.array
     """
-    return None
+    outdegree = np.zeros(len(A))
+    P = np.zeros(shape=(len(A), len(A[0])))
+    for i in range(len(A)):
+        for k in range(len(A[0])):
+            if A[i][k] != 0:
+                outdegree[i] += A[i][k]
+        for j in range(len(A[0])):
+            P[i][j] = A[j][i] / outdegree[i]
+    newA = np.matmul(np.transpose(np.identity(len(A)) - alpha * P), A/np.linalg.norm(A))
+    B = v
+    return np.linalg.solve(newA, B)
+
 
 
 def pageRankPower(A, alpha, v):
@@ -32,4 +43,7 @@ def pageRankPower(A, alpha, v):
 
 
 if __name__ == '__main__':
-    print('Hello, world!\n')
+    adj = np.genfromtxt('Adjacency_matrix.csv', delimiter=',')
+    pers = np.genfromtxt('VecteurPersonnalisation_Groupe11.csv', delimiter=',')
+    print(pageRankLinear(adj, 0.9, pers))
+    #print(sum(pageRankLinear(adj, 0.9, pers)))
