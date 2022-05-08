dd010c53ac7e418f8c1b8d68242b9007fb0bb4ea
Emile Villette <emile.villette@student.uclouvain.be>
2022-05-08 23:45:10 +0200

    a few comments to spice things up

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ def pageRankLinear(A, alpha, v):
     :rtype: np.array
     """
     outdegree = np.zeros(len(A))
+
+    # Determine the transition probabilities matrix from a and the out-degree of a
     P = np.zeros(shape=(len(A), len(A[0])))
     for i in range(len(A)):
         for k in range(len(A[0])):
@@ -22,6 +24,8 @@ def pageRankLinear(A, alpha, v):
                 outdegree[i] += A[i][k]
         for j in range(len(A[0])):
             P[i][j] = A[i][j] / outdegree[i]
+
+    # solve the linear system from slide 144 of chapter 10 (see Moodle, author: Marco Saerens)
     return np.linalg.solve(np.transpose(np.identity(len(A)) - alpha * P), (1 - alpha) * v)
 
 
@@ -39,6 +43,8 @@ def pageRankPower(A, alpha, v):
     """
     x = np.copy(v)
     outdegree = np.zeros(len(A))
+
+    # Determine the transition probabilities matrix from a and the out-degree of a
     P = np.zeros(shape=(len(A), len(A[0])))
     for i in range(len(A)):
         for k in range(len(A[0])):
@@ -46,24 +52,32 @@ def pageRankPower(A, alpha, v):
                 outdegree[i] += A[i][k]
         for j in range(len(A[0])):
             P[i][j] = A[i][j] / outdegree[i]
+
+    # Get the Google matrix
     G = np.transpose(alpha * P + (1 - alpha) * np.ones(len(P)) * np.transpose(v))
-    tempX = np.copy(x)
+
+    # Copy to determine when to stop iterating
+    temp_x = np.copy(x)
+
+    # Iterate on the left eigenvector until it converges
     stop_loop = False
     while not stop_loop:
         x = np.matmul(G, x)
         for i in range(len(x)):
-            if abs(x[i] - tempX[i]) < 0.000001:
+            # We could add a tolerance parameter to the function (not specified in the instructions so not added here)
+            # Stops iterating when the difference between the previous matrix and the current is below a given threshold
+            if abs(x[i] - temp_x[i]) < 0.000001:
                 stop_loop = True
             else:
                 stop_loop = False
                 break
-        tempX = np.copy(x)
+        # Replace temp_x with new x
+        temp_x = np.copy(x)
     return x
 
 
 if __name__ == '__main__':
     adj = np.genfromtxt('Adjacency_matrix.csv', delimiter=',')
     pers = np.genfromtxt('VecteurPersonnalisation_Groupe11.csv', delimiter=',')
-    print(pageRankLinear(adj, 0.85, pers))
-    print(pageRankPower(adj, 0.85, pers))
-    # print(sum(pageRankLinear(adj, 0.9, pers)))
+    print(pageRankLinear(adj, 0.9, pers))
+    print(pageRankPower(adj, 0.9, pers))
